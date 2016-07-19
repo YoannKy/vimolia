7315cd298aa02ca664374d468654cf438033410e
Rukamaru <rukanizuka@gmail.com>
2016-07-20 00:36:56 +0200

    MAJ CSS suite

--- resources/views/forms/doctors.blade.php
+++ resources/views/forms/doctors.blade.php
@@ -3,30 +3,40 @@
 
 @section('content')
 @if(Sentinel::inRole('expert','administrateur'))
-	<h2>liste des médecins</h2>
+	<h2>Liste des médecins</h2>
+	<div class="listeMedecin">
 	{{ Form::open(array('route' => ['forms.doctors.add',$formId])) }}
 	@foreach($doctors as $index => $doctor)
-		{{$doctor->first_name}}
-		{{$doctor->last_name}}
-		{{ Form::label('doc_'.$doctor->id, 'Proposer ce médecin')}}
-	    {{Form::checkbox('doctors[]',$doctor->id,false,array('id'=>'doc_'.$doctor->id))}}
+		<p>
+			{{$doctor->first_name}}
+			{{$doctor->last_name}}
+		</p>
+		<p>
+			{{ Form::label('doc_'.$doctor->id, 'Proposer ce médecin')}}
+		    {{Form::checkbox('doctors[]',$doctor->id,false,array('id'=>'doc_'.$doctor->id))}}
+		</p>
 	    <br>
 	@endforeach
 	{{ Form::submit('valider')}}
 	{{ Form::close()}}
+	</div>
 @else
 	<h2>Liste des médecins proposés par l'expert</h2>
-	<h3>
-		Vous pouvez prendre contact avec un des medecins proposés puis sélectionner celui avec qui vous avez pris le rendez-vous
-	</h3>
+	<p class="sous-titre">
+		Vous pouvez prendre contact avec un des médecins proposés puis sélectionner celui avec lequel vous avez pris le rendez-vous.
+	</p>
+
+	<div class="listeMedecin">
 	{{ Form::open(array('route' => ['forms.doctors.choose',$formId])) }}
-	@foreach($doctors as $index => $doctor)	
-		{{$doctor->first_name}}
-		{{$doctor->last_name}}
-		{{ Form::radio('doctor', $doctor->id )}}
-		<br>
+	@foreach($doctors as $index => $doctor)
+		<p>
+			{{$doctor->first_name}}
+			{{$doctor->last_name}}
+			{{ Form::radio('doctor', $doctor->id )}}
+		</p>
 	@endforeach
 		{{ Form::submit('valider')}}
 	{{ Form::close()}}
+	</div>
 @endif
 @stop
