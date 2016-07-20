bec2c0e7676a9d20923ada615dd06c8946ea4d64
Rukamaru <rukanizuka@gmail.com>
2016-07-20 12:31:48 +0200

    CSS Liste medecins

--- resources/views/forms/doctors.blade.php
+++ resources/views/forms/doctors.blade.php
@@ -5,20 +5,32 @@
 @if(Sentinel::inRole('expert','administrateur'))
 	<h2>Liste des médecins</h2>
 	<div class="listeMedecin">
-	{{ Form::open(array('route' => ['forms.doctors.add',$formId])) }}
-	@foreach($doctors as $index => $doctor)
-		<p>
-			{{$doctor->first_name}}
-			{{$doctor->last_name}}
-		</p>
-		<p>
-			{{ Form::label('doc_'.$doctor->id, 'Proposer ce médecin')}}
-		    {{Form::checkbox('doctors[]',$doctor->id,false,array('id'=>'doc_'.$doctor->id))}}
-		</p>
-	    <br>
-	@endforeach
-	{{ Form::submit('valider')}}
-	{{ Form::close()}}
+		<div class="row">
+		{{ Form::open(array('route' => ['forms.doctors.add',$formId])) }}
+		
+		@foreach($doctors as $index => $doctor)
+			<div class="col-sm-6 col-md-4">
+				<center>
+					<div class="thumbnail">
+						
+						
+						<div class="caption">
+							<h3>{{$doctor->first_name}} {{$doctor->last_name}}</h3>
+							<img src="/images/photo.png" alt="..."><br><br>
+							<p>
+								{{ Form::label('doc_'.$doctor->id, 'Proposer ce médecin')}}
+				    			{{Form::checkbox('doctors[]',$doctor->id,false,array('id'=>'doc_'.$doctor->id))}}
+				    		</p>
+						</div>
+					</div>
+				</center>
+			</div>
+		@endforeach
+		</div>
+		<div class="validationChoixMedecin">
+			{{ Form::submit('valider')}}
+			{{ Form::close()}}
+		</div>
 	</div>
 @else
 	<h2>Liste des médecins proposés par l'expert</h2>
