3dec940a943697e481a517ee23049169d00ed37e
Rukamaru <rukanizuka@gmail.com>
2016-07-21 02:37:47 +0200

    CSS liste praticiens

--- resources/views/centaur/doctors/index.blade.php
+++ resources/views/centaur/doctors/index.blade.php
@@ -3,12 +3,35 @@
 
 @section('content')
 
-@foreach($doctors as $index => $doctor)
-	{{$doctor->first_name}}
-	@if(Sentinel::inRole('expert'))
-		{!! link_to(route('convs.create',$doctor->id),'contacter') !!}
-	@endif
-	<br>
+<h2>Liste des praticiens</h2>
+<div class="panel panel-default">
+  <table class="table">
+  <thead>
+      <tr>
+        <th>#</th>
+        <th>Prénom</th>
+        <th>Nom</th>
+        <th>Adresse</th>
+        <th>Métier</th>
+        <th>Note</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach($doctors as $index => $doctor)
+      @if($conv->satisfied)
+      <tr>
+        <td>{{$index}}</td>
+        <td>{{$doctor->first_name}}</td>
+        <td>{{$doctor->last_name}}</td>
+        <td>{{$doctor->address}}</td>
+        <td>{{$doctor->profession}}</td>
+        <td>note</td>
+      </tr>
+      @if(Sentinel::inRole('expert'))
+			{!! link_to(route('convs.create',$doctor->id),'Contacter') !!}
+		@endif
 @endforeach
-
+    </tbody>
+  </table>
+</div>
 @stop
